c0ff288241d2e073079df6271f027e791de851db
Sergey Harini <harini.ua@gmail.com>
2022-04-08 19:27:20 +0300

    Menu items rework

--- app/View/Components/Menu.php
+++ app/View/Components/Menu.php
@@ -17,65 +17,93 @@ class Menu extends Component
      */
     public function __construct()
     {
+        $this->build();
+    }
+
+    private function build()
+    {
         /** @var User $user */
         $user = \Auth::user();
 
-        $this->items = [
-            [
-                'name' => __('Dashboard'),
-                'href' => route('home'),
-                'image' => asset('assets/images/svg-icon/dashboard.svg'),
-                'guard' => true,
-            ],
-            [
-                'name' => __('Users'),
-                'image' => asset('assets/images/svg-icon/crm.svg'),
-                'guard' => true,
-                'items' => [
-                    [
-                        'name' => __('Users'),
-                        'href' => route('users.index'),
-                        'image' => asset('assets/images/svg-icon/customers.svg'),
-                        'guard' => true,
-                    ],
-                    [
-                        'name' => __('Admins'),
-                        'href' => route('admins.index'),
-                        'image' => asset('assets/images/svg-icon/customers.svg'),
-                        'guard' => true,
-                    ]
+        /** ----- Dashboard ----- */
+
+        $this->items[] = [
+            'name' => __('Dashboard'),
+            'href' => route('home'),
+            'image' => asset('assets/images/svg-icon/dashboard.svg'),
+            'guard' => true,
+        ];
+
+        /** ----- Users ----- */
+
+        $this->items[] = $user->hasRole('admin') ? [
+            'name' => __('Users'),
+            'image' => asset('assets/images/svg-icon/crm.svg'),
+            'guard' => true,
+            'items' => [
+                [
+                    'name' => __('Users'),
+                    'href' => route('users.index'),
+                    'image' => asset('assets/images/svg-icon/customers.svg'),
+                    'guard' => true,
+                ],
+                [
+                    'name' => __('Admins'),
+                    'href' => route('admins.index'),
+                    'image' => asset('assets/images/svg-icon/customers.svg'),
+                    'guard' => $user->hasRole('admin'),
                 ]
-            ],
-            [
-                'name' => __('Distributors'),
-                'href' => route('distributors.index'),
-                'image' => asset('assets/images/svg-icon/maps.svg'),
-                'guard' => $user->hasRole('admin'),
-            ],
-            [
-                'name' => __('Companies'),
-                'href' => route('companies.index'),
-                'image' => asset('assets/images/svg-icon/frontend.svg'),
-                'guard' => true,
-            ],
-            [
-                'name' => __('Statistics'),
-                'href' => route('statistics.index'),
-                'image' => asset('assets/images/svg-icon/charts.svg'),
-                'guard' => true,
-            ],
-            [
-                'name' => __('Reports'),
-                'href' => route('reports.index'),
-                'image' => asset('assets/images/svg-icon/reports.svg'),
-                'guard' => true,
-            ],
-            [
-                'name' => __('Settings'),
-                'href' => route('settings.index'),
-                'image' => asset('assets/images/svg-icon/settings.svg'),
-                'guard' => true,
-            ],
+            ]
+        ] : [
+            'name' => __('Users'),
+            'image' => asset('assets/images/svg-icon/crm.svg'),
+            'href' => route('users.index'),
+            'guard' => true,
+        ];
+
+        /** ----- Distributors ----- */
+
+        $this->items[] = [
+            'name' => __('Distributors'),
+            'href' => route('distributors.index'),
+            'image' => asset('assets/images/svg-icon/maps.svg'),
+            'guard' => $user->hasRole('admin'),
+        ];
+
+        /** ----- Companies ----- */
+
+        $this->items[] = [
+            'name' => __('Companies'),
+            'href' => route('companies.index'),
+            'image' => asset('assets/images/svg-icon/frontend.svg'),
+            'guard' => $user->hasAnyRole(['admin', 'distributor']),
+        ];
+
+        /** ----- Statistics ----- */
+
+        $this->items[] = [
+            'name' => __('Statistics'),
+            'href' => route('statistics.index'),
+            'image' => asset('assets/images/svg-icon/charts.svg'),
+            'guard' => true,
+        ];
+
+        /** ----- Reports ----- */
+
+        $this->items[] = [
+            'name' => __('Reports'),
+            'href' => route('reports.index'),
+            'image' => asset('assets/images/svg-icon/reports.svg'),
+            'guard' => true,
+        ];
+
+        /** ----- Settings ----- */
+
+        $this->items[] = [
+            'name' => __('Settings'),
+            'href' => route('settings.index'),
+            'image' => asset('assets/images/svg-icon/settings.svg'),
+            'guard' => $user->hasRole('admin'),
         ];
     }
 
