4a0d8c590b42d494251a1359ba8510b7d64cdc39
Sergey Harini <harini.ua@gmail.com>
2022-04-08 20:11:04 +0300

    Error views

--- resources/views/admin/errors/404.blade.php
+++ resources/views/admin/errors/404.blade.php
@@ -8,11 +8,11 @@
             <!-- Start col -->
             <div class="col-md-8 col-lg-6">
                 <div class="text-center">
-                    <img src="assets/images/logo.svg" class="img-fluid error-logo" alt="logo">
-                    <img src="assets/images/error/404.svg" class="img-fluid error-image" alt="404">
-                    <h4 class="error-subtitle mb-4">Oops! Page not Found</h4>
-                    <p class="mb-4">We did not find the page you are looking for. Please return to previous page or visit home page. </p>
-                    <a href="{{url('/')}}" class="btn btn-primary font-16"><i class="feather icon-home mr-2"></i> Go back to Dashboard</a>
+                    <img src="{{ assert('assets/images/logo.svg') }}" class="img-fluid error-logo" alt="logo">
+                    <img src="{{ assert('assets/images/error/404.svg') }}" class="img-fluid error-image" alt="404">
+                    <h4 class="error-subtitle mb-4">{{ __('Oops! Page not Found') }}</h4>
+                    <p class="mb-4">{{ __('We did not find the page you are looking for. Please return to previous page or visit home page.') }}</p>
+                    <a href="{{ url()->previous() }}" class="btn btn-primary font-16"><i class="feather icon-home mr-2"></i> {{ __('Go back to Dashboard') }}</a>
                 </div>
             </div>
             <!-- End col -->
