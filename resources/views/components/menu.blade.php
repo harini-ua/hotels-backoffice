80ca78ee61e40ebd63342ac6502f589e131b86fd
Sergey Harini <harini.ua@gmail.com>
2022-04-08 19:04:54 +0300

    Fixed menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -8,11 +8,11 @@
                     @foreach($items as $key => $item)
                         @if($item['guard'])
                         <li class="dropdown">
-                            <a href="{{isset($item['href']) ?? 'javaScript:void();' }}"><img src="{{$item['image']}}" class="img-fluid" alt="{{__($item['name'])}}">{{__($item['name'])}}</a>
+                            <a href="{{isset($item['href']) ? $item['href'] : 'javaScript:void();' }}"><img src="{{$item['image']}}" class="img-fluid" alt="{{__($item['name'])}}">{{__($item['name'])}}</a>
                             @if(isset($item['items']))
                                 <ul class="dropdown-menu">
                                     @foreach($item['items'] as $subItem)
-                                        @if($item['guard'])
+                                        @if($subItem['guard'])
                                         <li><a href="{{$subItem['href']}}"><img src="{{$subItem['image']}}" class="img-fluid" alt="{{__($subItem['name'])}}">{{__($subItem['name'])}}</a></li>
                                         @endif
                                     @endforeach
