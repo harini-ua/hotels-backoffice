88a1cfdc7575835f72547d2989b77867372f3a5d
Sergey Harini <harini.ua@gmail.com>
2022-05-10 20:11:19 +0300

    Some fixed

--- database/migrations/2022_04_14_152053_create_company_templates_table.php
+++ database/migrations/2022_04_14_152053_create_company_templates_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCompanyTemplateTable extends Migration
+class CreateCompanyTemplatesTable extends Migration
 {
     /**
      * Run the migrations.
