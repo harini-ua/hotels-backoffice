1d17ff41b7a395d6855e88ad894d35b5266540ca
Sergey Harini <harini.ua@gmail.com>
2022-06-15 15:27:25 +0300

    Fixed filter component

--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -1,5 +1,5 @@
 @php($uniq = uniqid())
-<div class="accordion" id="datatable-filters">
+<div class="accordion" id="{{ 'datatable-filters-'.$uniq }}">
     <div class="card m-b-10">
         <div class="card-header" id="{{ 'heading-'.$uniq }}">
             <h2 class="mb-0">
@@ -13,7 +13,7 @@
                 ><i class="feather icon-filter mr-2"></i>{{ __($title) }}</button>
             </h2>
         </div>
-        <div id="{{ 'collapse-'.$uniq }}" class="collapse @if(!$collapse) show @endif" aria-labelledby="{{ 'heading-'.$uniq }}" data-parent="#datatable-filters" style="">
+        <div id="{{ 'collapse-'.$uniq }}" class="collapse @if(!$collapse) show @endif" aria-labelledby="{{ 'heading-'.$uniq }}" data-parent="{{ '#datatable-filters-'.$uniq }}" style="">
             <div class="card-body">
                 <div class="form-row filter-items-wrapper">
                     {{ $slot }}
