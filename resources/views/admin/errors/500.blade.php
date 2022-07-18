0c4a7d5f2b4c037530bb99aed8fc2965fc2b83cc
Sergey Harini <harini.ua@gmail.com>
2022-07-18 18:07:05 +0300

    Fixed 500 error page

--- resources/views/admin/errors/500.blade.php
+++ resources/views/admin/errors/500.blade.php
@@ -1,5 +1,5 @@
 @extends('admin.layouts.auth')
-@section('title',  __('500 Internal Server Error'))
+@section('title',  __('500 | Internal Server Error'))
 
 @section('content')
     <div class="auth-box error-box">
@@ -8,11 +8,10 @@
             <!-- Start col -->
             <div class="col-md-8 col-lg-6">
                 <div class="text-center">
-                    <img src="{{ assert('assets/images/logo.svg') }}" class="img-fluid error-logo" alt="logo">
-                    <img src="{{ assert('assets/images/error/internal-server.svg') }}" class="img-fluid error-image" alt="500">
+                    <img src="{{ asset('assets/images/error/internal-server.svg') }}" class="img-fluid error-image" alt="500">
                     <h4 class="error-subtitle mb-4">{{ __('500 Internal Server Error') }}</h4>
                     <p class="mb-4">{{ __('The server encountered an internal error or misconfiguration and was unable to complete your request.') }}</p>
-                    <a href="{{ url()->previous() }}" class="btn btn-primary font-16"><i class="feather icon-home mr-2"></i> {{ __('Go back to Dashboard') }}</a>
+                    <a href="{{ url()->previous() }}" class="btn btn-submit font-16"><i class="feather icon-home mr-2"></i> {{ __('Go Back') }}</a>
                 </div>
             </div>
             <!-- End col -->
