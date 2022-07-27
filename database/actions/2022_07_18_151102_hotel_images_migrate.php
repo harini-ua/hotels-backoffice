6390dfc2a6546763db5d4eee91f3debf7bb9a2c0
Sergey Harini <harini.ua@gmail.com>
2022-07-27 12:24:15 +0300

    Hotel primary image migrate

---
+++ database/actions/2022_07_18_151102_hotel_images_migrate.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Models\Hotel;
+use DragonCode\LaravelActions\Support\Actionable;
+use Ramsey\Uuid\Uuid;
+
+return new class extends Actionable
+{
+    /** @var array */
+    protected $environment = ['local', 'staging'];
+
+    /** @var array */
+    protected $except_environment = ['production'];
+
+    /** @var array */
+    protected $fields = [
+        'primary_image_url'
+    ];
+
+    /**
+     * Run the actions.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        $hotels = Hotel::all();
+        foreach ($hotels as $hotel) {
+            foreach ($this->fields as $field) {
+                if ($hotel->{$field}) {
+                    $file = storage_path($hotel->{$field});
+                    $extension = pathinfo(storage_path($hotel->{$field}), PATHINFO_EXTENSION);
+
+                    $destination = storage_path('app/public/hotels/'.$hotel->id.'/');
+                    $fileName = Uuid::uuid1().'.'.$extension;
+                    Storage::copy($file, $destination.$fileName);
+
+                    $hotel->update([ $field => $fileName ]);
+                }
+            }
+        }
+    }
+
+    /**
+     * Reverse the actions.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        //
+    }
+};
